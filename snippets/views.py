dcb91ef445f004ff303a5184a199d794b3f1ad86
Erik Westrup <erik.westrup@gmail.com>
2016-06-04 18:23:36 +0200

    Use api route

--- snippets/views.py
+++ snippets/views.py
@@ -177,9 +177,9 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserSerializer
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
+# @api_view(['GET'])
+# def api_root(request, format=None):
+    # return Response({
+        # 'users': reverse('user-list', request=request, format=format),
+        # 'snippets': reverse('snippet-list', request=request, format=format)
+    # })
